1b6338eae4343bf7c58afab488854cc60eb11841
Randa-10 <randa010011012@gmail.com>
2024-08-04 13:03:29 +0300

    day1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
+  standalone: true,  //v17 ,v18
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'Standalone';
+
 }
