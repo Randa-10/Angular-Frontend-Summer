c1bfcf347fefff08704065fe494cdf56add19774
Randa-10 <randa010011012@gmail.com>
2024-08-06 12:17:25 +0300

    Day3

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { ProductsComponent } from './components/products/products.component';
+import { NotfoundComponent } from './components/notfound/notfound.component';
 
 export const routes: Routes = [
 //first match wins
@@ -9,7 +10,10 @@ export const routes: Routes = [
   {path:"Home",component:HomeComponent,title:"home page"},
   {path:"Products",component:ProductsComponent,title:"product page"},
 
+  {path:'**',component:NotfoundComponent}
 
-  // not found page, wildcard path
-  {path:"**",component:HomeComponent},
+  // // not found page, wild card path
+
+
+
 ];
